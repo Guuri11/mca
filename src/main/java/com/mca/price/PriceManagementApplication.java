77d5ca2ca7f617f7bf0db54e27ce05e824372c04
Sergio Gurillo <sergio.gurillo11@gmail.com>
2023-06-02 18:31:31 +0200

    first commit

---
+++ src/main/java/com/mca/price/PriceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mca.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceManagementApplication.class, args);
+	}
+
+}
